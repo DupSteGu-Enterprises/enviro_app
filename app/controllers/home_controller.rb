77f6cd74c680008a530d4a2841c48c22240a2ff3
njdup <njdupoux1994@gmail.com>
2014-06-25 21:07:20 -0700

    Initialized site skeleton and routed to home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+    # Content for the home page
+    def index
+    end
+    
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enviro::Application.config.secret_key_base = 'fdc089ccc0632dc5634e1bf154c6c914128c7e34a2fc3713b0a25fabb403352c0831403ceed43d7a445ad1d5c4801cbdbea936b49aa3ca2fb65b18a476c44593'
